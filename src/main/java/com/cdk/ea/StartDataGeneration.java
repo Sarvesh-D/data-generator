bd77639ca82f0ef79e55cf8ad6895fadde1cf0ea
Sarvesh Dubey <sarvesh.dubey@hotmail.com>
2017-01-24 17:20:06 +0530

    Displaying time taken to generate data.

--- src/main/java/com/cdk/ea/StartDataGeneration.java
+++ src/main/java/com/cdk/ea/StartDataGeneration.java
@@ -1,15 +1,16 @@
 package com.cdk.ea;
 
 import java.util.Arrays;
-import java.util.Collection;
 
 import org.apache.commons.lang.StringUtils;
 
 import com.cdk.ea.data.core.Constants;
-import com.cdk.ea.data.generators.DataCollector;
 import com.cdk.ea.data.generators.DataGenerator;
 import com.cdk.ea.data.query.json.JsonQueryBuilder;
 
+import lombok.extern.java.Log;
+
+@Log
 public class StartDataGeneration {
     
     private StartDataGeneration() {
@@ -18,7 +19,8 @@ public class StartDataGeneration {
 
     public static void main(String... args) {
 	String finalCMDQuery;
-	Collection<DataCollector> dataCollected;
+	
+	long start = System.nanoTime();
 	
 	if("json".equals(args[0])) {
 	    String[] jsonFiles = Arrays.stream(args).filter(arg -> arg.endsWith(".json")).toArray(size -> new String[size]);
@@ -27,8 +29,12 @@ public class StartDataGeneration {
 	else
 	    finalCMDQuery = StringUtils.join(args, Constants.SPACE);
 	
-	dataCollected = DataGenerator.from(finalCMDQuery).generate();
-	dataCollected.stream().forEach(System.out::println);
+	DataGenerator.from(finalCMDQuery).generate();
+
+	long end = System.nanoTime();
+	double timeTaken = (end - start)/1000000000.0;
+	
+	log.info("Time Taken to generate Data : "+timeTaken+" secs");
     }
 
 }
