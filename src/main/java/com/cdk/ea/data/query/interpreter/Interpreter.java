74a83d77aac35d2e554d682a0ae6bc460dc61569
Sarvesh Dubey <sarvesh.dubey@hotmail.com>
2017-01-28 16:09:01 +0530

    Default implementation for doInterpret

--- src/main/java/com/cdk/ea/data/query/interpreter/Interpreter.java
+++ src/main/java/com/cdk/ea/data/query/interpreter/Interpreter.java
@@ -4,6 +4,8 @@ import com.cdk.ea.data.query.Query.QueryBuilder;
 
 interface Interpreter {
     
-    void doInterpret(QueryBuilder queryBuilder, String... identifiers);
+    default void doInterpret(QueryBuilder queryBuilder, String... identifiers) {
+	return;
+    }
 
 }
